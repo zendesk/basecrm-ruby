ec9596fe43646ea28e26fd5015fafdbec3dc3445
iaintshine <bodziomista@gmail.com>
2015-05-28 15:12:52 +0200

    add the low level SyncService service to BaseCRM::Client

--- lib/basecrm.rb
+++ lib/basecrm.rb
@@ -37,6 +37,7 @@ require 'basecrm/services/stages_service'
 require 'basecrm/services/tags_service'
 require 'basecrm/services/tasks_service'
 require 'basecrm/services/users_service'
+require 'basecrm/services/sync_service'
 
 module BaseCRM
   class Client
@@ -66,121 +67,129 @@ module BaseCRM
       @http_client = HttpClient.new(@config)
     end
 
-    # Access all Accounts related actions. 
-    # @see AccountsService 
+    # Access all Accounts related actions.
+    # @see AccountsService
     # @see Account
-    # 
+    #
     # @return [AccountsService] Service object for  resources.
     def accounts
       @accounts ||= AccountsService.new(@http_client)
     end
 
-    # Access all AssociatedContacts related actions. 
-    # @see AssociatedContactsService 
+    # Access all AssociatedContacts related actions.
+    # @see AssociatedContactsService
     # @see AssociatedContact
-    # 
+    #
     # @return [AssociatedContactsService] Service object for  resources.
     def associated_contacts
       @associated_contacts ||= AssociatedContactsService.new(@http_client)
     end
 
-    # Access all Contacts related actions. 
-    # @see ContactsService 
+    # Access all Contacts related actions.
+    # @see ContactsService
     # @see Contact
-    # 
+    #
     # @return [ContactsService] Service object for  resources.
     def contacts
       @contacts ||= ContactsService.new(@http_client)
     end
 
-    # Access all Deals related actions. 
-    # @see DealsService 
+    # Access all Deals related actions.
+    # @see DealsService
     # @see Deal
-    # 
+    #
     # @return [DealsService] Service object for  resources.
     def deals
       @deals ||= DealsService.new(@http_client)
     end
 
-    # Access all Leads related actions. 
-    # @see LeadsService 
+    # Access all Leads related actions.
+    # @see LeadsService
     # @see Lead
-    # 
+    #
     # @return [LeadsService] Service object for  resources.
     def leads
       @leads ||= LeadsService.new(@http_client)
     end
 
-    # Access all LossReasons related actions. 
-    # @see LossReasonsService 
+    # Access all LossReasons related actions.
+    # @see LossReasonsService
     # @see LossReason
-    # 
+    #
     # @return [LossReasonsService] Service object for  resources.
     def loss_reasons
       @loss_reasons ||= LossReasonsService.new(@http_client)
     end
 
-    # Access all Notes related actions. 
-    # @see NotesService 
+    # Access all Notes related actions.
+    # @see NotesService
     # @see Note
-    # 
+    #
     # @return [NotesService] Service object for  resources.
     def notes
       @notes ||= NotesService.new(@http_client)
     end
 
-    # Access all Pipelines related actions. 
-    # @see PipelinesService 
+    # Access all Pipelines related actions.
+    # @see PipelinesService
     # @see Pipeline
-    # 
+    #
     # @return [PipelinesService] Service object for  resources.
     def pipelines
       @pipelines ||= PipelinesService.new(@http_client)
     end
 
-    # Access all Sources related actions. 
-    # @see SourcesService 
+    # Access all Sources related actions.
+    # @see SourcesService
     # @see Source
-    # 
+    #
     # @return [SourcesService] Service object for  resources.
     def sources
       @sources ||= SourcesService.new(@http_client)
     end
 
-    # Access all Stages related actions. 
-    # @see StagesService 
+    # Access all Stages related actions.
+    # @see StagesService
     # @see Stage
-    # 
+    #
     # @return [StagesService] Service object for  resources.
     def stages
       @stages ||= StagesService.new(@http_client)
     end
 
-    # Access all Tags related actions. 
-    # @see TagsService 
+    # Access all Tags related actions.
+    # @see TagsService
     # @see Tag
-    # 
+    #
     # @return [TagsService] Service object for  resources.
     def tags
       @tags ||= TagsService.new(@http_client)
     end
 
-    # Access all Tasks related actions. 
-    # @see TasksService 
+    # Access all Tasks related actions.
+    # @see TasksService
     # @see Task
-    # 
+    #
     # @return [TasksService] Service object for  resources.
     def tasks
       @tasks ||= TasksService.new(@http_client)
     end
 
-    # Access all Users related actions. 
-    # @see UsersService 
+    # Access all Users related actions.
+    # @see UsersService
     # @see User
-    # 
+    #
     # @return [UsersService] Service object for  resources.
     def users
       @users ||= UsersService.new(@http_client)
+    end
+
+    # Access Sync API related low-level actions.
+    # @see SyncService
+    #
+    # @return [SyncService] Service object for Sync API.
+    def sync
+      @sync ||= SyncService.new(@http_client)
     end
   end
 end

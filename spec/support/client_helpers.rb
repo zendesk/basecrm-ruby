28149e58f97841bec5d23ce7db6dffdf0741b76e
Michal Sapka <msapka@gmail.com>
2016-06-21 15:52:31 +0200

    Add BASECRM_URL env variable

--- spec/support/client_helpers.rb
+++ spec/support/client_helpers.rb
@@ -1,11 +1,15 @@
 module ClientHelpers
   def client
-    @client ||= BaseCRM::Client.new(access_token: access_token)
+    @client ||= BaseCRM::Client.new(access_token: access_token, base_url: base_url)
   end
 
   def access_token
     @access_token ||= ENV.fetch("BASECRM_ACCESS_TOKEN") { raise '"BASECRM_ACCESS_TOKEN" has not been found.'  }
   end
+
+ def base_url
+    @basecrm_url ||= ENV.fetch("BASECRM_BASE_URL") { "https://api.getbase.com" }
+ end
 
   def contact
     @contact ||= FactoryGirl.create(:contact)

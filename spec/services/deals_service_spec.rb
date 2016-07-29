zendesk/basecrm-ruby

commit 78112f2aea497fc048ce1ae654156f1e298a1a8d

Coerce deal value to string before save

lib/basecrm/services/deals_service.rb
@@ -131,6 +131,7 @@ module BaseCRM
     end
        
     def sanitize(deal)
+      deal.value = Coercion.to_string(deal.value)
       deal.to_h.select { |k, _| OPTS_KEYS_TO_PERSIST.include?(k) }
     end
   end

lib/basecrm/utils/coercion.rb
@@ -5,5 +5,12 @@ module BaseCRM
     def self.to_decimal_value(value)
       BigDecimal.new((value || 0), 15)
     end
+
+    def self.to_string(value)
+      case value
+      when BigDecimal then value.to_s('F')
+      else value.to_s
+      end
+    end
   end
 end

spec/utils/coercion_spec.rb
@@ -1,0 +1,21 @@
+require "bigdecimal"
+require "spec_helper"
+
+describe BaseCRM::Coercion do
+  describe "to_decimal" do
+    it { expect(BaseCRM::Coercion.to_decimal_value(0)).to eql(0) }
+    it { expect(BaseCRM::Coercion.to_decimal_value("0")).to eql(0) }
+    it { expect(BaseCRM::Coercion.to_decimal_value(nil)).to eql(0) }
+    it { expect(BaseCRM::Coercion.to_decimal_value(1.1)).to eql(BigDecimal.new("1.1", 15)) }
+    it { expect(BaseCRM::Coercion.to_decimal_value("1.11")).to eql(BigDecimal.new("1.11", 15)) }
+  end
+
+  describe "to_string" do
+    it { expect(BaseCRM::Coercion.to_string(1)).to eql("1") }
+    it { expect(BaseCRM::Coercion.to_string("1")).to eql("1") }
+    it { expect(BaseCRM::Coercion.to_string(1.11)).to eql("1.11") }
+    it { expect(BaseCRM::Coercion.to_string("1.11")).to eql("1.11") }
+    it { expect(BaseCRM::Coercion.to_string(BigDecimal("1.11"))).to eql("1.11") }
+    it { expect(BaseCRM::Coercion.to_string(BigDecimal(1.11, 15))).to eql("1.11") }
+  end
+end

commit 43e5b8ce58ed7832540f23e4e6e068d84c798115

Fix deal creation with hash as argument
Fix a bug introduced in 78112f2 where it was impossible to create a deal
by just passing a hash to `create`. `sanitize` method was expecting
passed argument to respond to value which in case of hash was failing.

lib/basecrm/services/deals_service.rb
@@ -131,8 +131,9 @@ module BaseCRM
     end
        
     def sanitize(deal)
-      deal.value = Coercion.to_string(deal.value)
-      deal.to_h.select { |k, _| OPTS_KEYS_TO_PERSIST.include?(k) }
+      deal_hash = deal.to_h.select { |k, _| OPTS_KEYS_TO_PERSIST.include?(k) }
+      deal_hash[:value] = Coercion.to_string(deal_hash[:value])
+      deal_hash
     end
   end
 end

spec/services/deals_service_spec.rb
@@ -27,8 +27,13 @@ describe BaseCRM::DealsService do
 
   describe :create do
     it "returns instance of Deal class" do
-      @deal = build(:deal)
-      expect(client.deals.create(@deal)).to be_instance_of BaseCRM::Deal
+      deal = build(:deal)
+      expect(client.deals.create(deal)).to be_instance_of BaseCRM::Deal
+    end
+
+    it "allows to create a Deal with hash of parameters" do
+      deal = attributes_for(:deal)
+      expect(client.deals.create(deal)).to be_instance_of BaseCRM::Deal
     end
   end
 
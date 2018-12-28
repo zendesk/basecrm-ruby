85f1e316b03f5155d7d8c0df4301cba914ff4f30
Matt Jankowski <mjankowski@thoughtbot.com>
2018-12-28 12:48:10 -0500

    Replace deprecated `BigDecimal.new()` with `BigDecimal()`

--- spec/utils/coercion_spec.rb
+++ spec/utils/coercion_spec.rb
@@ -6,8 +6,8 @@ describe BaseCRM::Coercion do
     it { expect(BaseCRM::Coercion.to_decimal_value(0)).to eql(0) }
     it { expect(BaseCRM::Coercion.to_decimal_value("0")).to eql(0) }
     it { expect(BaseCRM::Coercion.to_decimal_value(nil)).to eql(0) }
-    it { expect(BaseCRM::Coercion.to_decimal_value(1.1)).to eql(BigDecimal.new("1.1", 15)) }
-    it { expect(BaseCRM::Coercion.to_decimal_value("1.11")).to eql(BigDecimal.new("1.11", 15)) }
+    it { expect(BaseCRM::Coercion.to_decimal_value(1.1)).to eql(BigDecimal("1.1", 15)) }
+    it { expect(BaseCRM::Coercion.to_decimal_value("1.11")).to eql(BigDecimal("1.11", 15)) }
   end
 
   describe "to_string" do
